10732bbf805135e3e4279e6fb0d010b895017f12
Lornatang <shiyipaisizuo@163.com>
2019-05-26 01:19:50 +0800

    init commit

--- Images/cnn.py
+++ Images/cnn.py
@@ -11,3 +11,58 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
+"""The most basic convolutional neural network is implemented by Tensorflow"""
+
+# import TensorFlow and other library.
+import tensorflow as tf
+from tensorflow.python.keras import datasets,layers,models
+
+# Download MNIST dataset
+(train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
+
+train_images = train_images.reshape((60000, 28, 28, 1))
+test_images = test_images.reshape((10000, 28, 28, 1))
+
+# Normalize pixel values to be between 0 and 1
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+
+# Create CNN model
+class CNN(models.Model):
+  def __init__(self):
+    super(CNN, self).__init__()
+    self.feature = models.Sequential([
+      layers.Conv2D(64, (3, 3), activation=tf.nn.relu, input_shape=(28, 28, 1)),
+      layers.MaxPool2D((2, 2)),
+      layers.Conv2D(64, (3, 3), activation=tf.nn.relu),
+      layers.MaxPool2D((2, 2)),
+      layers.Conv2D(64, (3, 3), activation=tf.nn.relu)
+    ])
+    self.classifier = models.Sequential([
+      layers.Flatten(),
+      layers.Dense(64, activation=tf.nn.relu),
+      layers.Dense(10, activation=tf.nn.softmax)
+    ])
+
+  def call(self, inputs, training=None, mask=None):
+    x = self.feature(inputs)
+    x = self.classifier(x)
+
+    return x
+
+
+# Load model and print model architecture.
+model = CNN()
+model.summary()
+
+# Compile and train the model
+model.compile(optimizer=tf.optimizers.Adam(),
+              loss=tf.losses.SparseCategoricalCrossentropy(),
+              metrics=['accuracy'])
+
+model.fit(train_images, train_labels, epochs=10)
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(test_images, test_labels)
+
+print(test_acc)
