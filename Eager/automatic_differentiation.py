172eaaf580c5ca332b898e9abd373fbf28acc324
Lornatang <shiyipaisizuo@163.com>
2019-05-26 09:59:38 +0800

    Automatic derivation of neural units

---
+++ Eager/automatic_differentiation.py
@@ -0,0 +1,103 @@
+# Copyright 2019 ChangyuLiu Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""In the previous tutorial we introduced Tensors and operations on them.
+In this tutorial we will cover automatic differentiation,
+a key technique for optimizing machine learning models.
+"""
+
+# Import TensorFlow and other library
+import tensorflow as tf
+
+# ==============================================================================
+# For example.
+# Gradient tapes
+x = tf.ones((2, 2))
+
+with tf.GradientTape() as t:
+  t.watch(x)
+  y = tf.reduce_sum(x)
+  z = tf.multiply(y, y)
+
+# Derivative of z with respect to the original input tensor x
+dz_dx = t.gradient(z, x)
+for i in [0, 1]:
+  for j in [0, 1]:
+    assert dz_dx[i][j].numpy() == 8.0
+
+# ==============================================================================
+# You can also request gradients of the output with respect to intermediate values
+# computed during a "recorded" tf.GradientTape context.
+x = tf.ones((2, 2))
+
+with tf.GradientTape() as t:
+  t.watch(x)
+  y = tf.reduce_sum(x)
+  z = tf.multiply(y, y)
+
+# Use the tape to compute the derivative of z with respect to the
+# intermediate value y.
+dz_dy = t.gradient(z, y)
+assert dz_dy.numpy() == 8.0
+# ==============================================================================
+
+x = tf.constant(3.0)
+with tf.GradientTape(persistent=True) as t:
+  t.watch(x)
+  y = x * x
+  z = y * y
+dz_dx = t.gradient(z, x)  # 108.0 (4*x^3 at x = 3)
+dy_dx = t.gradient(y, x)  # 6.0
+del t  # Drop the reference to the tape
+
+
+# Because tapes record operations as they are executed,
+# Python control flow (using ifs and whiles for example) is naturally handled
+def f(x, y):
+  output = 1.0
+  for i in range(y):
+    if 1 < i < 5:
+      output = tf.multiply(output, x)
+  return output
+
+
+def grad(x, y):
+  with tf.GradientTape() as t:
+    t.watch(x)
+    out = f(x, y)
+  return t.gradient(out, x)
+
+
+x = tf.convert_to_tensor(2.0)
+
+assert grad(x, 6).numpy() == 12.0
+assert grad(x, 5).numpy() == 12.0
+assert grad(x, 4).numpy() == 4.0
+
+# Higher-order gradients
+x = tf.Variable(1.0)  # Create a TensorFlow variable initialized to 1.0
+
+with tf.GradientTape() as t:
+  with tf.GradientTape() as t2:
+    y = x * x * x
+  # Compute the gradient inside the 't' context manager
+  # which means the gradient computation is differentiable as well.
+  dy_dx = t2.gradient(y, x)
+d2y_dx2 = t.gradient(dy_dx, x)
+
+assert dy_dx.numpy() == 3.0
+assert d2y_dx2.numpy() == 6.0
+
+
+
