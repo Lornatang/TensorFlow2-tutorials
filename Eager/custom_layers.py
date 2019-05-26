04d0087fc58ac207dfd8fa33c570ce27751c8ca7
Lornatang <shiyipaisizuo@163.com>
2019-05-26 08:13:12 +0800

    Explained how to implement ResNet using a custom model layer

---
+++ Eager/custom_layers.py
@@ -0,0 +1,115 @@
+# Copyright 2019 ChangyuLiu Authors. All Rights Reserved.
+# Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+"""We recommend using tf.keras as a high-level API for building neural networks.
+That said, most TensorFlow APIs are usable with eager execution.
+"""
+
+# Import TensorFlow and other library
+import tensorflow as tf
+
+# Layers: common sets of useful operations
+# In the tf.keras.layers package, layers are objects. To construct a layer,
+# simply construct the object. Most layers take as a first argument the number
+# of output dimensions / channels.
+"""layer = tf.keras.layers.Dense(100)"""
+# The number of input dimensions is often unnecessary, as it can be inferred
+# the first time the layer is used, but it can be provided if you want to
+# specify it manually, which is useful in some complex models.
+layer = tf.keras.layers.Dense(10, input_shape=(None, 5))
+
+# It includes Dense (a fully-connected layer), Conv2D, LSTM, BatchNormalization,
+# Dropout, and many others.
+# To use a layer, simply call it.
+layer(tf.zeros([10, 5]))
+
+# Layers have many useful methods. For example, you can inspect all variables
+# in a layer using `layer.variables` and trainable variables using
+# `layer.trainable_variables`. In this case a fully-connected layer
+# will have variables for weights and biases.
+print(layer.variables)
+
+print(layer.kernel, layer.bias)
+
+
+# Implementing custom layers
+class MyDenseLayer(tf.keras.layers.Layer):
+  def __init__(self, input_shape, num_outputs):
+    super(MyDenseLayer, self).__init__()
+    self.kernel = self.add_variable("kernel",
+                                    shape=[int(input_shape[-1]),
+                                           self.num_outputs])
+    self.num_outputs = num_outputs
+
+  def build(self, input_shape):
+    pass
+
+  def call(self, inputs, **kwargs):
+    return tf.matmul(inputs, self.kernel)
+
+
+layer = MyDenseLayer([-1], 10)
+print(layer(tf.zeros([10, 5])))
+print(layer.trainable_variables)
+
+
+# Models: composing layers
+class ResnetIdentityBlock(tf.keras.Model):
+  def __init__(self, kernel_size, filters):
+    super(ResnetIdentityBlock, self).__init__(name='')
+    filters1, filters2, filters3 = filters
+
+    self.conv2a = tf.keras.layers.Conv2D(filters1, (1, 1))
+    self.bn2a = tf.keras.layers.BatchNormalization()
+
+    self.conv2b = tf.keras.layers.Conv2D(filters2, kernel_size, padding='same')
+    self.bn2b = tf.keras.layers.BatchNormalization()
+
+    self.conv2c = tf.keras.layers.Conv2D(filters3, (1, 1))
+    self.bn2c = tf.keras.layers.BatchNormalization()
+
+  def call(self, input_tensor, training=False, mask=None):
+    x = self.conv2a(input_tensor)
+    x = self.bn2a(x, training=training)
+    x = tf.nn.relu(x)
+
+    x = self.conv2b(x)
+    x = self.bn2b(x, training=training)
+    x = tf.nn.relu(x)
+
+    x = self.conv2c(x)
+    x = self.bn2c(x, training=training)
+
+    x += input_tensor
+    return tf.nn.relu(x)
+
+
+block = ResnetIdentityBlock(1, [1, 2, 3])
+print(block(tf.zeros([1, 2, 3, 3])))
+print([x.name for x in block.trainable_variables])
+
+# Much of the time, however, models which compose many layers simply call
+# one layer after the other. This can be done in very little code using
+# tf.keras.Sequential
+my_seq = tf.keras.Sequential([tf.keras.layers.Conv2D(1, (1, 1), input_shape=(None, None, 3)),
+                             tf.keras.layers.BatchNormalization(),
+                             tf.keras.layers.Conv2D(2, 1, padding='same'),
+                             tf.keras.layers.BatchNormalization(),
+                             tf.keras.layers.Conv2D(3, (1, 1)),
+                             tf.keras.layers.BatchNormalization()])
+my_seq(tf.zeros([1, 2, 3, 3]))
+
+
+
+
